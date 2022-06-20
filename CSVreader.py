17ddade8635c9ebb2a467d8f988b2a5be2aa1c53
Martin <m.hein090@gmail.com>
2022-06-20 11:59:51 +0100

    Changed delimiter

--- CSVreader.py
+++ CSVreader.py
@@ -6,6 +6,7 @@ Date last modified: 02/06/2022
 Version: 1.01
 Python Version: 3.9
 """
+
 import csv
 
 
@@ -13,11 +14,13 @@ class CSVreader:
     @staticmethod
     def read_csv(path):
         with open(path, newline='') as csvfile:
-            file_reader = csv.reader(csvfile, delimiter=' ')
+            file_reader = csv.reader(csvfile, delimiter='`')
             file_rows = [x[0].split(',') for x in file_reader]
             file_data = {}
-            for i in range(0, len(file_rows[0])-1):
+            for i in range(0, len(file_rows[0])):
                 key = file_rows[0][i]
+                if len(key) <= 0:
+                    continue
                 values = []
                 for o in range(1, len(file_rows)):
                     values.append(file_rows[o][i])
